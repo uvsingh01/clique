0e87c88723e056904a09634b3ce166a26b087f71
Yuvraj Singh <mr.uvsingh@gmail.com>
2023-06-29 22:43:42 +0530

    homepage

--- src/components/home/Nav.js
+++ src/components/home/Nav.js
@@ -4,7 +4,7 @@ const Nav = () => {
   return (
    <nav className='fixed bg-white opacity-90 h-14 w-full z-50'>
     <a href='/' className='h-full w-full '>
-        <img src='./logo.svg' className='h-full' alt="Zinacor" />
+        <img src='./logo.svg' className='h-full pl-6' alt="Zinacor" />
     </a>
    </nav>
   )
